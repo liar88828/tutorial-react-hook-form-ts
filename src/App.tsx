ef6c9694e04d52581176189e4fd8c0a34b832e7b
brian <liar88828@gmail.com>
2023-04-30 23:45:58 +0700

    2 Form setup

---
+++ src/App.tsx
@@ -0,0 +1,13 @@
+import './style/App.css'
+import { YoutubeForm } from './components/eps2/YoutubeForm'
+
+function App() {
+
+  return (
+    <>
+<YoutubeForm/>      
+    </>
+  )
+}
+
+export default App
